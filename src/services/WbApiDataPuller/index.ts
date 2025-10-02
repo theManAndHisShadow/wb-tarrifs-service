396b9ce3490d77ab4da4e7126820546b185954b2
theManAndHisShadow <justemailpostbox@gmail.com>
2025-10-02 20:30:49 +0500

    [Service]: wrapped api request to 'getFreshDataFormWbAPI()' function.

--- src/services/WbApiDataPuller/index.ts
+++ src/services/WbApiDataPuller/index.ts
@@ -2,20 +2,20 @@ import 'dotenv/config';
 import { convertToHumanDate, getValueFromEnv } from "#utils/common.js";
 
 // N.B: draft code
-const API_KEY = getValueFromEnv('DONT_LET_THE_KEY_LEAK_OR_YOU_WILL_FIRED');
-const API_ENDPOINT = "https://common-api.wildberries.ru/api/v1/tariffs/box";
+export async function getFreshDataFormWbAPI(endpoint: string) {
+  const API_KEY = getValueFromEnv('DONT_LET_THE_KEY_LEAK_OR_YOU_WILL_FIRED');
 
-const today = convertToHumanDate({
-  timestamp: Date.now(),
-  reversed: true,
-  delimiter: '-'
-});
+  const today = convertToHumanDate({
+    timestamp: Date.now(),
+    reversed: true,
+    delimiter: '-'
+  });
 
-const response = await fetch(`${API_ENDPOINT}?date=${today}`, {
-  headers: {
-    "Authorization": API_KEY, 
-  }
-});
+  const response = await fetch(`${endpoint}?date=${today}`, {
+    headers: {
+      "Authorization": API_KEY,
+    }
+  });
 
-const data = await response.json();
-console.dir(data, { depth: null, colors: true });
+  return await response.json();
+}
