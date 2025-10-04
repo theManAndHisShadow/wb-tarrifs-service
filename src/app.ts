e89c245d5f1513142ba9a997c55caf234d373384
theManAndHisShadow <justemailpostbox@gmail.com>
2025-10-04 18:49:25 +0500

    [Service]: added sync at startup.

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,18 @@ await seed.run();
 
 console.log("All migrations and seeds have been run");
 
+// sync at startup
+(async () => {
+  console.log(`[TASK][${new Date().toISOString()}]: Initial sync to Google Sheets at startup`);
+  try {
+    await pullAndPush();
+    console.log(`[TASK][${new Date().toISOString()}]: ✅ Initial sync completed`);
+  } catch (err) {
+    console.error(`[TASK][${new Date().toISOString()}]: ❌ Initial sync failed`, err);
+  }
+})();
+
 cron.schedule(`0 * * * *`, async () => {
-  console.log(`[${new Date().toISOString()}] Syncing to Google Sheets`);
+  console.log(`[TASK][${new Date().toISOString()}]: Syncing to Google Sheets`);
   await pullAndPush();
 });
