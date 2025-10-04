84d5f9cf815e7aa9c3bed30cfbf6f54494716066
theManAndHisShadow <justemailpostbox@gmail.com>
2025-10-04 11:42:28 +0500

    [Service]: crone is now configured

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,17 @@
+import cron from 'node-cron';
 import knex, { migrate, seed } from "#postgres/knex.js";
+import pullAndPush from '#tasks/pullAndPush.js';
+import { getValueFromEnv } from '#utils/common.js';
 
 await migrate.latest();
 await seed.run();
 
 console.log("All migrations and seeds have been run");
+
+// в тестовом режиме интервал - каждую минуту
+const interval = getValueFromEnv('CRON_INTERVAL');
+
+cron.schedule(`*/${interval} * * * *`, async () => {
+  console.log(`[${new Date().toISOString()}] Syncing to Google Sheets`);
+  await pullAndPush();
+});
